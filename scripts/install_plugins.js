16f842d981f823a722b427ea01d1a6b4afa135f4
lenteweb <webmaster@lenteweb.com>
2016-08-15 13:52:42 -0500

    brouser
    corregi plugin

--- scripts/install_plugins.js
+++ scripts/install_plugins.js
@@ -10,6 +10,7 @@ var pluginlist = [
     "org.apache.cordova.device-motion",
     "org.apache.cordova.device-orientation",
     "org.apache.cordova.geolocation",
+	"org.apache.cordova.inappbrowser",
     "https://github.com/chrisekelley/AppPreferences/"
 ];
 
